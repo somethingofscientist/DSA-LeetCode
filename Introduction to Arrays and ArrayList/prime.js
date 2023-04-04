b25c85895575294cd024da40feaa1841f09e794a
Shraddhanand <1shraddhanand@gmail.com>
2023-04-04 15:10:30 +0530

    prime numbeR

---
+++ Introduction to Arrays and ArrayList/prime.js
@@ -0,0 +1,11 @@
+function prime(x) {
+    if (x < 2) return 'no prime number'
+    for (let i = 2; i < Math.sqrt(x); i++) {
+        if (x % i === 0) {
+            return 'no prime number'
+        }
+    }
+    return 'yes prime number'
+}
+
+console.log(prime(1))
--- Introduction to Recursion/index.js
+++
@@ -1,6 +0,0 @@
-function app(n) {
-    if(n == 51) return n
-    console.log(n)
-    return app(n - 1)
-}
-console.log(app(5))
