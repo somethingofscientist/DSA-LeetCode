69c33c5b2e4680e10c6fac7873a42169a462d60d
Shraddhanand <1shraddhanand@gmail.com>
2023-04-04 11:13:04 +0530

    recursion

--- Introduction to Recursion/Intro.js
+++ Introduction to Recursion/Intro.js
@@ -1,1 +1,18 @@
-let a = 6
+function msg1(){
+    console.log('hello world_1')
+    msg2()
+}
+function msg2(){
+    console.log('hello world_2')
+    msg3()
+}
+function msg3(){
+    console.log('hello world_3')
+    msg4()
+}
+function msg4(){
+    console.log('hello world_4')
+    // msg2()
+}
+
+msg1()
