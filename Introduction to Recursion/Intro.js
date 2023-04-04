fb7863411715fc23967d14cf7e039c39adc0f29f
Shraddhanand <1shraddhanand@gmail.com>
2023-04-04 12:15:14 +0530

    s

--- Introduction to Recursion/Intro.js
+++ Introduction to Recursion/Intro.js
@@ -1,18 +1,11 @@
-function msg1(){
-    console.log('hello world_1')
-    msg2()
-}
-function msg2(){
-    console.log('hello world_2')
-    msg3()
-}
-function msg3(){
-    console.log('hello world_3')
-    msg4()
-}
-function msg4(){
-    console.log('hello world_4')
-    // msg2()
-}
+// counting in recursion 
 
-msg1()
+const recursiveLoop = (x) => {
+    // console.log(x); for decreasin order
+    if (x > 1) {
+        recursiveLoop(x - 1);
+    };
+    return x;
+    // console.log(x) for increasing order
+}
+recursiveLoop(5);
